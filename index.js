5df3c9a450a4b66f5dda562272bd30648344939d
haider_ali <innoxentmughalforever@gmail.com>
2024-02-13 08:34:36 +0500

    project complete

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-require('dotenv').config();
+const { MongoClient } = require("mongodb")
 const express = require('express');
+const urlparser = require("url")
 const cors = require('cors');
-const app = express();
+require('dotenv').config();
+const net = require('net');
 const dns = require("dns")
-var bodyParser = require('body-parser')
+const app = express();
+
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db("urlshortner")
+const urls = db.collection("urls")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -29,26 +35,79 @@ app.post('/api/shorturl', (req, res) => {
     family: 6,
     hints: dns.ADDRCONFIG | dns.V4MAPPED,
   };
-  const hostname = new URL(url).hostname;
-
-  dns.lookup(hostname, ip6Option, (err, address, family) => {
+  console.log(url)
+  debugger
+  // const { port, hostname } = new URL(url)
+  const { hostname } = urlparser.parse(url)
+  dns.lookup(hostname, async (err, address) => {
     if (err) {
-      res.json({ "error": "Invalid URL" })
+      res.json({ error: "Invalid Url" })
     }
     else {
-      const short_url = Math.random().toString(36).substring(2, 8)
-      res.json({ original_url: url, short_url: short_url })
+      const urlCount = await urls.countDocuments();
+      const urlDoc = {
+        url: url,
+        shortUrl: urlCount
+      }
+      const result = await urls.insertOne(urlDoc)
+      console.log(result);
+      res.json({ original_url: url, short_url: urlCount })
     }
-    // console.log('address: %j family: IPv%s', address, family)
   })
+
+  // dns.lookup(hostname, ip6Option, (err, address, family) => {
+  //   if (err) {
+  //     res.json({ "error": "Invalid URL" })
+  //   }
+  //   else {
+
+  //     const socket = new net.Socket();
+  //     socket.setTimeout(5000); // set a timeout in milliseconds
+
+  //     socket.on('connect', () => {
+  //       console.log(`Connection to ${address}:${port} successful.`);
+  //       const shorUrlGenerate = (url) => {
+  //         let shortUrl = 0;
+  //         for (let key in url) {
+  //           shortUrl += key.charCodeAt();
+  //         }
+  //         return shortUrl
+  //       }
+  //       const short_url = shorUrlGenerate(url)
+  //       res.json({ original_url: url, short_url: short_url })
+  //       socket.end();
+  //     });
+
+  //     socket.on('timeout', () => {
+  //       console.error(`Connection to ${address}:${port} timed out.`);
+  //       socket.destroy();
+  //     });
+
+  //     socket.on('error', (error) => {
+  //       console.error(`Error connecting to ${address}:${port}:`, error);
+  //       socket.destroy();
+  //     });
+
+  //     socket.connect(port, address);
+
+
+  //   }
+  //   // console.log('address: %j family: IPv%s', address, family)
+  // })
 
 })
 
-app.get('/api/shorturl/:id', (req, res) => {
+app.get('/api/shorturl/:short_url', async (req, res) => {
   debugger
-  const { baseUrl,url } = req
+  try {
+    const { short_url } = req.params
+    //convert string into integer with plus
+    const urlDoc = await urls.findOne({shortUrl:+short_url})
+    res.redirect(urlDoc.url)
+  } catch (error) {
+    console.log(error)
+  }
 
-  res.redirect(url)
 })
 
 app.listen(port, function () {
